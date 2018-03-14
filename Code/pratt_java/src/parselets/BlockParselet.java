8aaaafb24cec4e158470ff3b19acc7ed3080c2be
Alan R <alanribeiroandrade@gmail.com>
2018-03-14 21:26:48 +0100

    Added BlockParselet

---
+++ Code/pratt_java/src/parselets/BlockParselet.java
@@ -0,0 +1,32 @@
+package parselets;
+
+import expressions.Expression;
+import lexer.Token;
+import lexer.TokenType;
+import parser.Parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parses parentheses used to group an expression, like "a * (b + c)".
+ */
+public class BlockParselet {
+    public ArrayList<Expression> parse(Parser parser, Token token) {
+        ArrayList<Expression> expressions = new ArrayList<>();
+        while (true) {
+
+            // Stop when we hit the closing }.
+            if (parser.match(TokenType.TOK_CLOSE_CURLY)) break;
+            expressions.add(parser.parseStatement());
+
+
+            //if (parser.match(TokenType.TOK_SEMI_COLON)) break;
+
+            // Each expression is separated by a line.
+            //parser.consume(TokenType.TOK_SEMI_COLON);
+        }
+
+        return expressions;
+    }
+}
