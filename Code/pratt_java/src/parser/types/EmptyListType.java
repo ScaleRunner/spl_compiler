a72e42e4c8ba020f359726e5bba39fa546869068
Alan R <alanribeiroandrade@gmail.com>
2018-04-25 08:53:13 +0200

    Added EmptyListType

---
+++ Code/pratt_java/src/parser/types/EmptyListType.java
@@ -0,0 +1,40 @@
+package parser.types;
+
+import typechecker.Substitution;
+
+public class EmptyListType extends Type {
+
+    private static EmptyListType instance = null;
+
+    private EmptyListType(){
+
+    }
+
+    public static EmptyListType getInstance(){
+        if(instance == null){
+            instance = new EmptyListType();
+        }
+        return instance;
+    }
+
+    @Override
+    public Type applySubstitution(Substitution substitution) {
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Empty";
+    }
+}
