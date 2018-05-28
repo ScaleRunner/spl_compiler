57a7234a4bcafc6022430a810b6675585fa29efa
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-29 01:25:58 +0200

    addToOutput now needs to know if it is the end of the line

--- Code/pratt_java/src/codeGeneration/python/ProgramWriter.java
+++ Code/pratt_java/src/codeGeneration/python/ProgramWriter.java
@@ -9,6 +9,7 @@ public class ProgramWriter {
 
     private final String filepath;
     private final List<String> program;
+    private String currLine;
 
     // For keeping track of how many indents we have to do
     private String currIndent;
@@ -21,10 +22,15 @@ public class ProgramWriter {
         this.program = new ArrayList<>();
         this.indent = indent;
         this.currIndent = "";
+        this.currLine = "";
     }
 
-    public void addToOutput(String line){
-        this.program.add(line);
+    public void addToOutput(String line, boolean EoL){
+        this.currLine += line + " ";
+        if(EoL){
+            this.program.add(this.currIndent + this.currLine);
+            this.currLine = "";
+        }
     }
 
     public void addIndent(){
@@ -33,6 +39,11 @@ public class ProgramWriter {
 
     public void removeIndent(){
         this.currIndent = this.currIndent.replace(this.indent, "");
+    }
+
+    public void printLast(){
+        String lastLine = this.program.remove(program.size());
+        this.program.add(String.format("print(%s)", lastLine));
     }
 
     public void writeToFile() throws FileNotFoundException {
