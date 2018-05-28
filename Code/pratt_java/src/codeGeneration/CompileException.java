346ab419707c1ed4dedb0ae44643bbd288ffa70f
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-29 00:48:50 +0200

    Throw strings only for compile exceptions

--- Code/pratt_java/src/codeGeneration/CompileException.java
+++ Code/pratt_java/src/codeGeneration/CompileException.java
@@ -7,4 +7,8 @@ public class CompileException extends RuntimeException {
     public CompileException(String message, Node n) {
         super(String.format("%s\n\tError found in line %s", message, n));
     }
+
+    public CompileException(String message) {
+        super(message);
+    }
 }
