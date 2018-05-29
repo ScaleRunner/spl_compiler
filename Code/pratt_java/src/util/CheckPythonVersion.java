c47c0390e4a5738eda9948fb69d311aa86d7f194
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-29 23:49:32 +0200

    Fixed a bug when "python3" is not in your path

--- Code/pratt_java/src/util/CheckPythonVersion.java
+++ Code/pratt_java/src/util/CheckPythonVersion.java
@@ -1,11 +1,6 @@
 package util;
 
-import codeGeneration.CompileException;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,20 +11,12 @@ public class CheckPythonVersion {
             command.add("python3");
             command.add("--version");
             ProcessBuilder builder = new ProcessBuilder(command);
-            final Process process = builder.start();
-            InputStream is = process.getErrorStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-
-            process.waitFor();
-
-            String line = br.readLine();
-            return line == null ? "python3" : "python";
+            builder.start();
+            // If the code reaches this point, we can assume that the command is successful
+            return "python3";
 
         } catch (IOException e) {
-            throw new CompileException("Stream could not be opened/closedn\n" + e.getMessage());
-        } catch (InterruptedException e) {
-            throw new CompileException("Python stopped abruptly\n" + e.getMessage());
+            return "python";
         }
     }
 }
