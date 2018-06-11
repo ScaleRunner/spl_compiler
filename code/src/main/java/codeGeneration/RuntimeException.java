561ed661e7b03cf0da90d7fbe24c9e04fe73ea8d
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-06-12 00:23:37 +0200

    Implemented RuntimeExceptions

---
+++ code/src/main/java/codeGeneration/RuntimeException.java
@@ -0,0 +1,9 @@
+package codeGeneration;
+
+@SuppressWarnings("serial")
+public class RuntimeException extends java.lang.RuntimeException {
+
+    public RuntimeException(String message) {
+        super(message);
+    }
+}
