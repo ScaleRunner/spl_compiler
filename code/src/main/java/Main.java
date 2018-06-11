e94e1c09e935e486a48d391892a7ed7df70df1c3
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-06-12 00:23:57 +0200

    Added the main file

---
+++ code/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import cli.CLI;
+import cli.Runner;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CLI cli = new CLI(args);
+
+        try {
+            CommandLine cmd = cli.parse();
+            Runner runner = new Runner(cmd);
+            runner.execute();
+        } catch (ParseException e) {
+            cli.help();
+        } catch (FileNotFoundException e) {
+            System.err.println("The inputfile could not be found!");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Execution halted abruptly!");
+        } catch (IOException e) {
+            System.err.println("The outputfile could not be created! Do you have the correct rights?\n");
+            e.printStackTrace();
+        }
+
+    }
+}
